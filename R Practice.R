4e55be25d7c762e2a5178af567cefd82b43ad2a2
Lauren Esser <Lauren.Esser02@gmail.com>
2021-01-18 11:09:54 -0500

    Learning R

---
+++ R Practice.R
@@ -0,0 +1,17 @@
+2 + 2
+my_number <- 2 + 2
+my_number * 2
+coffees <- c(2, 1, 2, 3, 1, 2, 0, 2, 3, 1)
+coffees * 1.2
+adjusted_coffees <-coffees * 1.2
+coffee_data <- data.frame(coffees, adjusted_coffees)
+coffee_data
+coffee_data[1,]
+coffee_data[,1]
+coffee_data[1,1]
+coffee_data$coffees
+sum(2,2)
+lsf("package:base")
+lsf("package:base")
+dplry::if_else
+dplyr::if_else
